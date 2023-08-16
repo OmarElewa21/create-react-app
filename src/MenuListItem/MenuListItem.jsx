c1658600afdf507eea2c1a199f57b8b0f23a2a6c
Omar Elewa <omarelewa25@gmail.com>
2023-08-17 02:25:37 +0300

    add menu list items module

---
+++ src/MenuListItem/MenuListItem.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import style from './MenuListItem.module.css'
+
+export const MenuListItem = props => {
+  const [isHovered, setIsHovered] = useState(false)
+  return (
+    <div
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+      className={`${style.container} ${
+        isHovered ? style.hovered : style.notHovered
+      } ${props.currentDifficulty === props.difficulty ? style.selected : ''}`}
+      onClick={() => props.handleDifficultyChange(props.difficulty)}
+    >
+      Set to: {props.difficulty}{' '}
+    </div>
+  )
+}
